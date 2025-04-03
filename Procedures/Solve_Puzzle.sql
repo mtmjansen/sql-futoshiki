8161845dea8da008e21d866eacb08e1bcdf7aa91
mtmjansen <>
2025-04-03 22:11:02 +0200

    Solve_Puzzle
    loops over Reduce_Options

---
+++ Procedures/Solve_Puzzle.sql
@@ -0,0 +1,39 @@
+USE Futoshiki
+GO
+
+ALTER PROCEDURE Solve_Puzzle --
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE --
+		@OptionsBefore int = 7*7*7,
+		@OptionsAfter int = 1,
+		@CellSolved int = 0
+
+	WHILE @OptionsBefore > @OptionsAfter AND @OptionsAfter > 0
+	BEGIN
+		SELECT @OptionsBefore = count(*) FROM [dbo].[Options]
+
+		EXEC [dbo].[Reduce_Options]
+
+		SELECT @OptionsAfter = count(*) FROM [dbo].[Options]
+
+		IF(@OptionsBefore <> @OptionsAfter)
+		BEGIN
+			EXEC [dbo].[Show_Status] 1
+		END
+	END
+
+	IF @OptionsAfter > 0
+	BEGIN
+		THROW 50000, 'I give up!', 0
+	END
+
+	SELECT @CellSolved = count(*) FROM Puzzle
+	IF @CellSolved < 7*7
+	BEGIN
+		THROW 50000, 'I failed! :-(', 0
+	END 
+END
+GO
