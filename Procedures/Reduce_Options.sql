29358cb11af2d5eba1a51ee46b4469983f854fa1
mtmjansen <>
2025-04-03 23:10:47 +0200

    use greater than and less than hints

--- Procedures/Reduce_Options.sql
+++ Procedures/Reduce_Options.sql
@@ -4,6 +4,40 @@ GO
 ALTER PROCEDURE Reduce_Options
 AS
 BEGIN
+	; WITH cte_must_be_gt AS (
+		SELECT --
+			h.idGreater,
+			smallest = min(lesser.answer)
+		FROM dbo.Hints h
+		INNER JOIN dbo.Options lesser --
+		ON lesser.Id = h.idLesser
+		GROUP BY --
+			h.idLesser, 
+			h.idGreater
+	)
+	DELETE o
+	FROM Options o
+	RIGHT JOIN cte_must_be_gt c
+	ON c.idGreater = o.id
+	AND NOT c.smallest < o.answer
+
+	; WITH cte_must_be_lt AS (
+		SELECT --
+			h.idLesser, 
+			largest = max(greater.answer)
+		FROM dbo.Hints h
+		INNER JOIN dbo.Options greater
+		ON greater.Id = h.idGreater
+		GROUP BY --
+			h.idLesser, 
+			h.idGreater
+	)
+	DELETE o
+	FROM Options o
+	RIGHT JOIN cte_must_be_lt c
+	ON c.idLesser = o.id
+	AND NOT c.largest > o.answer
+
 	; WITH cells_with_one_option AS (
 		SELECT id
 		FROM Options
